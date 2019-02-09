1781adc52ff98994e9d091cf04b8fd0e90bb5f91
gt60 <gt60@ya.ru>
2019-02-09 21:51:12 +0200

    Webpack optimization

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,6 @@ const webpack = require('webpack')
 
 const config = {
   entry: [
-    'tether',
     'font-awesome/scss/font-awesome.scss',
     './src/app.js'
   ],
@@ -41,17 +40,6 @@ const config = {
       'window.Tether': 'tether',
       Popper: ['popper.js', 'default'],
       'window.Tether': 'tether',
-      Alert: 'exports-loader?Alert!bootstrap/js/dist/alert',
-      Button: 'exports-loader?Button!bootstrap/js/dist/button',
-      Carousel: 'exports-loader?Carousel!bootstrap/js/dist/carousel',
-      Collapse: 'exports-loader?Collapse!bootstrap/js/dist/collapse',
-      Dropdown: 'exports-loader?Dropdown!bootstrap/js/dist/dropdown',
-      Modal: 'exports-loader?Modal!bootstrap/js/dist/modal',
-      Popover: 'exports-loader?Popover!bootstrap/js/dist/popover',
-      Scrollspy: 'exports-loader?Scrollspy!bootstrap/js/dist/scrollspy',
-      Tab: 'exports-loader?Tab!bootstrap/js/dist/tab',
-      Tooltip: "exports-loader?Tooltip!bootstrap/js/dist/tooltip",
-      Util: 'exports-loader?Util!bootstrap/js/dist/util'
     }),
     new CopyWebpackPlugin([
       {
