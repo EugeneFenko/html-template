581701b0a99f122c491512006bf4e094abc9186f
gt60 <gt60@ya.ru>
2019-02-15 21:28:57 +0200

    Use SSH for deploy

--- deploy.github.js
+++ deploy.github.js
@@ -2,7 +2,7 @@ var ghpages = require('gh-pages');
 
 ghpages.publish('dist', {
     branch: 'master',
-    repo: 'https://github.com/EugeneFenko/eugenefenko.github.io.git'
+    repo: 'git@github.com:EugeneFenko/eugenefenko.github.io.git'
   }, function(err){
     console.log('Err: '+err)
   });
