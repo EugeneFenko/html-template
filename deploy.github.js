dff0001bd72c2d29dc22a610f1413d5bcd3b1742
gt60 <gt60@ya.ru>
2019-03-16 18:06:19 +0200

    Fix deploy script

--- deploy.github.js
+++ deploy.github.js
@@ -4,5 +4,8 @@ ghpages.publish('dist', {
     branch: 'master',
     repo: 'https://github.com/EugeneFenko/eugenefenko.github.io.git'
   }, function(err){
-    console.log('Err: '+err)
+    if(err==undefined) {
+      console.log('Success!')
+    } 
+    else console.log('Err: '+err)
   });
